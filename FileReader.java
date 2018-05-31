82d0a1254ee2b024f1d2b058ea108f0895d412ea
polon59 <przemyslaw.polczak@gmail.com>
2018-05-31 02:27:04 +0200

    implement file reader method in class FileReader.java

--- FileReader.java
+++ FileReader.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+import java.io.File;
 
 public class FileReader {
 
-    
+    private String fileText;
+
+    public void readDataFile(){
+        File data = new File("data.csv");
+        StringBuilder text = new StringBuilder();
+
+        try{
+            Scanner scanner = new Scanner(data);
+            while (scanner.hasNextLine()){
+                text.append(scanner.nextLine());
+                text.append("\n");    
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            }
+
+        this.fileText = text.toString();    
+        System.out.println(this.fileText);
+    }
 }
