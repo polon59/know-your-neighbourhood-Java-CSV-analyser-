30ec8bc9638700044566f37e75b1cdccbe702efe
polon59 <przemyslaw.polczak@gmail.com>
2018-06-02 18:02:13 +0200

    reconstruct method 'findLongestString' to handle two table columns

--- Display.java
+++ Display.java
@@ -6,21 +6,31 @@ import java.util.HashMap;
 
 public class Display {
 
-    private int findLongestString(ArrayList<String> list){
-        int longest = list.get(0).length();
-        int currentElementLength;
+    private int[] findLongestString(ArrayList<String> list){
+        int maxLenColumn1 = 0;
+        int maxLenColumn2 = 0;
+        int[] columnLengths = new int[2];
+
+        int currentWordLength;
+        String[] lineElements;
 
         for (String element : list){
-            currentElementLength = element.length();
-            if (currentElementLength > longest){
-                longest = currentElementLength;
+            lineElements = element.split("---");
+            if (lineElements[0].length() > maxLenColumn1){
+                maxLenColumn1 = lineElements[0].length();
+            }
+            if (lineElements[1].length() > maxLenColumn2){
+                maxLenColumn2 = lineElements[1].length();
             }
         }
-        return longest;
+        columnLengths[0] = maxLenColumn1;
+        columnLengths[1] = maxLenColumn2;
+        return columnLengths;
     }
 
     private int calculateLengthDifference(int maxLength, String stringToWrite) {
         int difference = maxLength - stringToWrite.length();
+        //System.out.println(difference);
         return difference;
     }
 
@@ -29,23 +39,29 @@ public class Display {
         int lengthDifference = calculateLengthDifference(maxLength, stringToWrite);
         row.append("|");
         row.append(stringToWrite);
+        //System.out.println(stringToWrite.length());
 
         for (int i = 0; i < lengthDifference; i ++){
             row.append(" ");
         }
 
 
-        row.append("|\n");
+        //row.append("|\n");
         return row;
     }
 
     public void printListInTable(ArrayList<String> list, String header){
         StringBuilder table = new StringBuilder();
-        int maxLength = findLongestString(list);
+        int[] maxLengths = findLongestString(list);
+        
+        String[] rowElements;
         //System.out.println(maxLength);
 
         for (String stringToWrite : list){
-            table.append(createTableRow(maxLength, stringToWrite)) ;
+            rowElements = stringToWrite.split("---");
+            table.append(createTableRow(maxLengths[0], rowElements[0])) ;
+            table.append(createTableRow(maxLengths[1], rowElements[1]));
+            table.append("|\n");
         }
         System.out.println(table.toString());
 
