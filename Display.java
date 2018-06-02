5b9d0a4bba9ee58aae4a693dd475b059f2c10657
polon59 <przemyslaw.polczak@gmail.com>
2018-06-02 03:42:34 +0200

    implement method 'findlongestString' in class display.java

--- Display.java
+++ Display.java
@@ -1,5 +1,29 @@
+import java.lang.StringBuilder;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Display {
 
+    private int findLongestString(ArrayList<String> list){
+        int longest = list.get(0).length();
+        int currentElementLength;
+
+        for (String element : list){
+            currentElementLength = element.length();
+            if (currentElementLength > longest){
+                longest = currentElementLength;
+            }
+        }
+        return longest;
+    }
+
+    private int calculateLengthDifference(int maxLength, String stringToWrite) {
+        int difference = maxLength - stringToWrite.length();
+        return difference;
+    }
+
+    
     
 }
